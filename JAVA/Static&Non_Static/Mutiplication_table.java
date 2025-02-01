9083333fa6cd8a522b7b43780708d1b047f1087b
Ann Mary Rifna <156402926+annmaryrifna@users.noreply.github.com>
2025-02-01 20:51:21 +0530

    Mutiplication_table

---
+++ JAVA/Static&Non_Static/Mutiplication_table.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Multiplication{
+       public static int Multiple(int n)
+ {
+        for(int i=0;i<10;i++)
+        {
+         m=i*n;
+         }
+         return m;
+
+    public static void main(String[] args) 
+{ 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        
+            Multiplt(num);
+        
+        sc.close();
+    }
+}
